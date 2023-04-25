34ef408960ac3288df0a9246c4df185914241f3b
lannam <lannam@islander.tamucc.edu>
2023-04-25 13:52:08 -0500

    Initial commit

---
+++ src/Screens/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+
+import './Home.css'
+export default function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <div className="banner" >
+      <div>
+        <h2>Welcome Gayathri Annam ! Looking for a job? </h2>
+        <h3 style={{color:"white"}}>
+          You are at the right place. Find Part Time and Full time jobs from different sectors.
+        </h3>
+      </div>
+      
+      
+    
+        </div>
+       
+        <div>
+        
+        </div>
+       
+    </div>
+  )
+}
