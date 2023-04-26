8841921270586adb5c6c1b427a860f2f211d1d5b
lannam <lannam@islander.tamucc.edu>
2023-04-26 13:53:44 -0500

    added new feature

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import ops from '../JobOps.gif'
+import premium from '../premium.gif'
 
 import './Home.css'
 export default function Home() {
@@ -13,13 +14,16 @@ export default function Home() {
        </div>
         <div className="banner" >
       <div>
-      <img src={ops} alt="GIF" style={{float:'left', marginLeft:'15px' ,marginTop:'-100px', width:'40%', height:'40%'}}/>
+      
         <h2>Welcome Gayathri Annam ! Looking for a job? </h2>
         <h3 style={{color:"white"}}>
           You are at the right place. Find Part Time and Full time jobs from different sectors.
         </h3>
       </div>
-      
+      <div className='container' style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+        <img src={ops} alt="GIF" style={{marginBottom: '15px', width:'40%', height:'40%'}}/>
+         <img src={premium} alt="GIF" style={{ width:'40%', height:'40%'}}/>
+          </div>
       
     
         </div>
